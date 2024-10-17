03509bf64ee4645a3bd3bd494ce94e2b623c6217
Mahfuzur Rahman <mxhfxzur.r@gmail.com>
2024-10-17 18:46:21 +0100

    add dark mode and light mode icons

--- frontend/src/routes/Settings.tsx
+++ frontend/src/routes/Settings.tsx
@@ -1,4 +1,6 @@
 import {useState, useContext, ChangeEvent, MouseEvent} from 'react'
+import { MdDarkMode } from "react-icons/md";
+import { MdLightMode } from "react-icons/md";
 
 export default function Settings() {
   const [isDarkModeEnabled, setIsDarkModeEnabled] = useState(false);
@@ -13,7 +15,7 @@ export default function Settings() {
       onClick={handleToggleDarkMode}
       className="flex items-center justify-center w-fit ml-auto mr-auto mt-10 gap-5 bg-gray-300 p-8 rounded-md"
     >
-      {isDarkModeEnabled ? 'On' : 'Off'}
+      {isDarkModeEnabled ? <MdDarkMode size={40} /> : <MdLightMode size={40} />}
     </button>
   );
 }
