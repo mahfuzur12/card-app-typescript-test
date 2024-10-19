5d5ade4ac1619fdc6fba12963a989138ddcffc1c
Mahfuzur Rahman <mxhfxzur.r@gmail.com>
2024-10-19 21:20:14 +0100

    background colour change when dark mode is enabled - (next step - need to save darkmode state)

--- frontend/src/routes/Settings.tsx
+++ frontend/src/routes/Settings.tsx
@@ -1,9 +1,17 @@
-import {useState, useContext, ChangeEvent, MouseEvent} from 'react'
+import {useState, useContext, ChangeEvent, MouseEvent, useEffect} from 'react'
 import { MdDarkMode } from "react-icons/md";
 import { MdLightMode } from "react-icons/md";
 
 export default function Settings() {
   const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    if (darkMode) {
+      document.body.classList.add("dark");
+    } else {
+      document.body.classList.remove("dark");
+    }
+  }, [darkMode]);
 
   const handleToggleDarkMode = () => {
     setDarkMode(!darkMode);
